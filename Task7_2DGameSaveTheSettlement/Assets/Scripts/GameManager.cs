2efffd14897acaf162aad77ddbda47e1953606fb
Зайцев-Андрей <andrey_zaycev_2000@mail.ru>
2023-06-14 14:14:24 +0300

    completed the project

--- Task7_2DGameSaveTheSettlement/Assets/Scripts/GameManager.cs
+++ Task7_2DGameSaveTheSettlement/Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement; // для перезапуска игры
 
 public class GameManager : MonoBehaviour
 {
@@ -19,6 +21,29 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Text _enemiesSurgeCounterText;
     [SerializeField] private Text _counterOfEnemiesInNewSurgeText;
 
+    // Для конца игры
+    // По умолчанию всё настроено для поражения, поэтому подготавливаем все компоненты для смены на победу
+    // (152, 64, 64) - цвет для поражения
+    // (102, 152, 64) - цвет победы
+    [SerializeField] private GameObject _gameOverScreen;
+    [SerializeField] private Image _imageWithStats;
+    [SerializeField] private Sprite _greenImageForWin;        
+    [SerializeField] private Text _statsWithNumbersForGameOver;
+    [SerializeField] private Text _gameOverText;
+
+    [SerializeField] private GameObject _pauseScreen;
+
+    [SerializeField] private Button _switchSoundButton;
+    private Image _currentSpriteOfSoundButton;
+    [SerializeField] private Sprite _soundOn;
+    [SerializeField] private Sprite _soundOff;
+
+    [SerializeField] private AudioSource _buttonClickSound;
+    [SerializeField] private AudioSource _enviromentMusic;
+    [SerializeField] private AudioSource _foodSound;
+    [SerializeField] private AudioSource _wheatSound;
+    [SerializeField] private AudioSource _battleSound;
+
     private int _wheatCounter;
     private int _peasantCounter = 2;
     private int _warriorsCounter;
@@ -31,6 +56,10 @@ public class GameManager : MonoBehaviour
     private int _peasentPrice = 3;
     private int _warriorPrice = 5;
 
+    private int _numberOfDeathWarriors;
+    private int _numberOfAllHiringWarriors;
+    private int _numberOfPassedSurges;
+
     private bool _isRecruitmentingOfPeasant = false;
     private bool _isRecruitmentingOfWarrior = false;
 
@@ -39,17 +68,35 @@ public class GameManager : MonoBehaviour
     private int _counterOfEnemies = 0;
     private float _addTimeToNewSurge;
 
+    private int _wheatForWin = 400;
+    private int _peasantForWin = 30;
+
+    private bool isPause = false;
+
     void Start()
     {
         _peasantCounterText.text = _peasantCounter.ToString();
         _addTimeToNewSurge = _enemiesWatchHandler._maxTime * 0.6f;
+
+        _currentSpriteOfSoundButton = _switchSoundButton.GetComponent<Image>();
+        _currentSpriteOfSoundButton.sprite = _soundOn;
     }
 
     void Update()
     {
+        if(isPause == false && Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseOnClick();
+        }
+        else if (isPause == true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            ContinueOnCLick();
+        }
+
         if(_addToWheatWatchHandler.isTick)
         {
             _wheatCounter += _peasantCounter * 3;
+            _wheatSound.Play();
 
             if (_hirePeasant.interactable == false && _wheatCounter >= _peasentPrice && _isRecruitmentingOfPeasant == false)
                 _hirePeasant.interactable = true;
@@ -62,11 +109,20 @@ public class GameManager : MonoBehaviour
         {
             // 2 - это то, сколько пшена съедает каждый воин и крестьянин
             _wheatCounter -= (_warriorsCounter * 2 + _peasantCounter * 2);
+            _foodSound.Play();
         }
 
         if(_enemiesWatchHandler.isTick)
         {
+            _battleSound.pitch = UnityEngine.Random.Range(0.8f, 1.2f);
+            _battleSound.Play();           
+
+            if (_warriorsCounter >= _counterOfEnemies)
+                _numberOfDeathWarriors += _counterOfEnemies;
+            else _numberOfDeathWarriors += _warriorsCounter;
+
             _warriorsCounter -= _counterOfEnemies;
+            
             _counterOfSurges++;
             if (_counterOfSurges >= 4)
             {
@@ -76,7 +132,6 @@ public class GameManager : MonoBehaviour
             }
             _enemiesSurgeCounterText.text = _counterOfSurges.ToString();
             _counterOfEnemiesInNewSurgeText.text = _counterOfEnemies.ToString();
-            // тут будет проверка на отрицательное кол-во воинов - в таком случае поражение
         }
 
         if (_hirePeasant.interactable == true && _wheatCounter < _peasentPrice)
@@ -110,21 +165,49 @@ public class GameManager : MonoBehaviour
             _currentWarriorTimer = -2;
             _timerForRecruitmentWarrior.fillAmount = 1;
             _warriorsCounter++;
+            _numberOfAllHiringWarriors++;
             _isRecruitmentingOfWarrior = false;
         }
 
         UpdateText();
+
+        if(_warriorsCounter < 0)
+        {
+            _numberOfPassedSurges = _counterOfSurges - 2;
+            ActiveGameOverScreen();
+        }
+        else if (_wheatCounter >= _wheatForWin || _peasantCounter >= _peasantForWin)
+        {
+            // Сначала меняем цвет экранов для победы на зелёный
+            _gameOverScreen.GetComponent<Image>().color = new Color(102f / 255, 152f / 255, 64f / 255, 180f / 255);
+            _imageWithStats.sprite = _greenImageForWin;
+            _gameOverText.text = "Вы победили!!!";
+            _numberOfPassedSurges = _counterOfSurges - 1;
+            ActiveGameOverScreen();
+        }
+
     }
 
     private void UpdateText()
     {
         _wheatCounterText.text = _wheatCounter.ToString();
         _peasantCounterText.text = _peasantCounter.ToString();
-        _warriorsCounterText.text = _warriorsCounter.ToString();
+        if (_warriorsCounter >= 0)
+            _warriorsCounterText.text = _warriorsCounter.ToString();
+        else _warriorsCounterText.text = "0";
+    }
+
+    private void ActiveGameOverScreen()
+    {
+        _statsWithNumbersForGameOver.text =
+                $"{Math.Round(Time.time)}\n\n{_wheatCounter}\n\n{_peasantCounter}\n\n{_numberOfAllHiringWarriors}\n\n\n{_numberOfPassedSurges}\n\n{_numberOfDeathWarriors}";
+        Time.timeScale = 0;
+        _gameOverScreen.SetActive(true);
     }
 
     public void HirePeasant()
     {
+        _buttonClickSound.Play();
         _hirePeasant.interactable = false;
         _currentPeasantTimer = _peasantCreateTime;
         _wheatCounter -= _peasentPrice;
@@ -133,9 +216,60 @@ public class GameManager : MonoBehaviour
 
     public void HireWarrior()
     {
+        _buttonClickSound.Play();
         _hireWarrior.interactable = false;
         _currentWarriorTimer = _warriorCreateTime;
         _wheatCounter -= _warriorPrice;
         _isRecruitmentingOfWarrior = true;
+    }
+
+    public void SwitchSound()
+    {
+        if (_currentSpriteOfSoundButton.sprite == _soundOn)
+        {
+            _currentSpriteOfSoundButton.sprite = _soundOff;
+            _enviromentMusic.Pause();
+            _buttonClickSound.volume = 0f;
+            _foodSound.volume = 0f;
+            _wheatSound.volume = 0f;
+            _battleSound.volume = 0f;
+        }
+
+        else
+        {
+            _currentSpriteOfSoundButton.sprite = _soundOn;
+            _enviromentMusic.Play();
+            _buttonClickSound.volume = 0.3f;
+            _foodSound.volume = 0.15f;
+            _wheatSound.volume = 0.2f;
+            _battleSound.volume = 0.05f;
+        }
+    }
+
+    public void PauseOnClick()
+    {
+        isPause = true;
+        _pauseScreen.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ContinueOnCLick()
+    {
+        isPause = false;
+        _pauseScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void RestartOnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+    }
+
+    public void PlayAndPauseOnClick()
+    {
+        if (isPause)
+            ContinueOnCLick();
+        else PauseOnClick();
     }
 }
