c50f77c52f4fbf09620e92d731301dbdb70b84f3
Зайцев-Андрей <andrey_zaycev_2000@mail.ru>
2023-06-16 16:59:20 +0300

    Improve code in scripts

--- Task8_MovingAndRotationOfObjects/Assets/Scripts/MovingAndRotationForRocket.cs
+++ Task8_MovingAndRotationOfObjects/Assets/Scripts/MovingAndRotationForRocket.cs
@@ -6,37 +6,19 @@ public class MovingAndRotationForRocket : MonoBehaviour
 {
     [SerializeField] private Transform _rocketTransform;
 
-    [SerializeField] private Transform _point1Transform;
-    [SerializeField] private Transform _point2Transform;
-    [SerializeField] private Transform _point3Transform;
-    [SerializeField] private Transform _point4Transform;
-    [SerializeField] private Transform _point5Transform;
-    [SerializeField] private Transform _point6Transform;
-    [SerializeField] private Transform _point7Transform;
+    [SerializeField] private Vector3[] _allPoints;
 
     [SerializeField] private float _rotationSpeed;
     [SerializeField] private float _movingSpeed;
 
-    private List<Transform> _allPoints;
     private int _currentTargetIndex;
     private Vector3 _currentTarget;
     private bool _forwardMoving;
 
     void Start()
     {
-        _allPoints = new List<Transform>()
-        {
-            _point1Transform,
-            _point2Transform,
-            _point3Transform,
-            _point4Transform,
-            _point5Transform,
-            _point6Transform,
-            _point7Transform
-        };
-
         _currentTargetIndex = 0;
-        _currentTarget = _allPoints[_currentTargetIndex].position;
+        _currentTarget = _allPoints[_currentTargetIndex];
         _rocketTransform.LookAt(_currentTarget);
     }
 
@@ -47,7 +29,7 @@ public class MovingAndRotationForRocket : MonoBehaviour
         _rocketTransform.position = Vector3.MoveTowards(_rocketTransform.position, _currentTarget, _movingSpeed * Time.deltaTime);
         if(_rocketTransform.position == _currentTarget)
         {
-            if (_currentTargetIndex + 1 == _allPoints.Count)
+            if (_currentTargetIndex + 1 == _allPoints.Length)
                 _forwardMoving = false;
             else if (_currentTargetIndex == 0)
                 _forwardMoving = true;
@@ -56,7 +38,7 @@ public class MovingAndRotationForRocket : MonoBehaviour
                 _currentTargetIndex++;
             else _currentTargetIndex--;
 
-            _currentTarget = _allPoints[_currentTargetIndex].position;
+            _currentTarget = _allPoints[_currentTargetIndex];
             _rocketTransform.LookAt(_currentTarget);
         }
     }
--- Task8_MovingAndRotationOfObjects/Assets/Scripts/MovingOfRunner.cs
+++ Task8_MovingAndRotationOfObjects/Assets/Scripts/MovingOfRunner.cs
@@ -4,55 +4,25 @@ using UnityEngine;
 
 public class MovingOfRunner : MonoBehaviour
 {
-    [SerializeField] private Transform _runner1;
-    [SerializeField] private Transform _runner2;
-    [SerializeField] private Transform _runner3;
-    [SerializeField] private Transform _runner4;
-    [SerializeField] private Transform _runner5;
-    [SerializeField] private Transform _runner6;
+    [SerializeField] private Transform[] _allRunners;
 
     // Организуем передачу эстафетной палочки
     [SerializeField] private Transform _stick;
-    [SerializeField] private Transform _placeForStickOfRunner1;
-    [SerializeField] private Transform _placeForStickOfRunner2;
-    [SerializeField] private Transform _placeForStickOfRunner3;
-    [SerializeField] private Transform _placeForStickOfRunner4;
-    [SerializeField] private Transform _placeForStickOfRunner5;
-    [SerializeField] private Transform _placeForStickOfRunner6;
+    [SerializeField] private Transform[] _allPlacesForStick;
 
     [SerializeField] private float _movingSpeed;
+    [SerializeField] private float _passDistance;
 
     private int _currentRunnerIndex;
     private int _currentTargetIndex;
     private Transform _currentRunner;
     private Vector3 _currentTarget;
-    private List<Transform> _allRunners;
-    private List<Transform> _allPlacesForStick;
 
     private Vector3 _startPosition;
     private bool _isMoving = true;
 
     void Start()
     {
-        _allRunners = new List<Transform>()
-        { 
-            _runner1,
-            _runner2,
-            _runner3,
-            _runner4,
-            _runner5,
-            _runner6,
-        };
-        _allPlacesForStick = new List<Transform>()
-        {
-            _placeForStickOfRunner1,
-            _placeForStickOfRunner2,
-            _placeForStickOfRunner3,
-            _placeForStickOfRunner4,
-            _placeForStickOfRunner5,
-            _placeForStickOfRunner6
-        };
-
         _currentRunnerIndex = 0;
         _currentTargetIndex = 1;
         _currentRunner = _allRunners[0];
@@ -65,10 +35,10 @@ public class MovingOfRunner : MonoBehaviour
     {
         _currentRunner.position = Vector3.MoveTowards(_currentRunner.position, _currentTarget, _movingSpeed * Time.deltaTime);
 
-        if(_isMoving && _currentRunner.position == _currentTarget)
+        if(_isMoving && Vector3.Distance(_currentRunner.position, _currentTarget) <= _passDistance)
         {
             _currentRunnerIndex++;
-            if (_currentTargetIndex + 1 == _allRunners.Count)
+            if (_currentTargetIndex + 1 == _allRunners.Length)
             {
                 _currentTarget = _startPosition;
                 _isMoving = false;
