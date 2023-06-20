e87ce758950417e21a408374b75b8f01bc0fe017
Зайцев-Андрей <andrey_zaycev_2000@mail.ru>
2023-06-20 17:34:11 +0300

    Added pinball board and script for pins

---
+++ Task10_PhysicsAndJoints/Assets/Scripts/PinMoving.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinMoving : MonoBehaviour
+{
+    [SerializeField] private HingeJoint _hingeJoint;
+    [SerializeField] private float _timeToHit;
+
+    private float _currentTimeToHit;
+
+    void Start()
+    {
+        _currentTimeToHit = _timeToHit;
+    }
+
+    void Update()
+    {
+        _currentTimeToHit -= Time.deltaTime;
+        JointSpring spring = _hingeJoint.spring;
+        if (_currentTimeToHit < 0)
+        {            
+            spring.targetPosition = -90f;
+            _hingeJoint.spring = spring;
+            _currentTimeToHit = _timeToHit;
+        }
+        else
+        {
+            spring.targetPosition = spring.targetPosition + (Time.deltaTime / _timeToHit * 90f);
+            _hingeJoint.spring = spring;
+        }
+    }
+}
